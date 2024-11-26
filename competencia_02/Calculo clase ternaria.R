b1de670f9f59f4d012b0b347dd7c3946435c454a
Julián Devouassoux <149077813+JulianDevouassoux@users.noreply.github.com>
2024-11-26 21:56:44 +0000

    Add files via upload

---
+++ competencia_02/Calculo clase ternaria.R
@@ -0,0 +1,55 @@
+# Arbol elemental con libreria  rpart
+# Debe tener instaladas las librerias  data.table ,  rpart  y  rpart.plot
+
+# cargo las librerias que necesito
+require("data.table")
+require("rpart")
+require("rpart.plot")
+
+# Aqui se debe poner la carpeta de la materia de SU computadora local
+setwd("C:/Users/julyh/OneDrive/Escritorio/dmeyf/datasets")
+
+# cargo el dataset que tiene la clase calculada !
+dataset <- fread("competencia_01_crudo.csv")
+
+# calculo el periodo0 consecutivo
+dsimple <- dataset[, list(
+  "pos" = .I,
+  numero_de_cliente,
+  periodo0 = as.integer(foto_mes/100)*12 +  foto_mes%%100 ) ]
+
+
+# ordeno
+setorder( dsimple, numero_de_cliente, periodo0 )
+
+# calculo topes
+periodo_ultimo <- dsimple[, max(periodo0) ]
+periodo_anteultimo <- periodo_ultimo - 1
+
+
+# calculo los leads de orden 1 y 2
+dsimple[, c("periodo1", "periodo2") :=
+          shift(periodo0, n=1:2, fill=NA, type="lead"),  numero_de_cliente ]
+
+# assign most common class values = "CONTINUA"
+dsimple[ periodo0 < periodo_anteultimo, clase_ternaria := "CONTINUA" ]
+
+# calculo BAJA+1
+dsimple[ periodo0 < periodo_ultimo &
+           ( is.na(periodo1) | periodo0 + 1 < periodo1 ),
+         clase_ternaria := "BAJA+1" ]
+
+# calculo BAJA+2
+dsimple[ periodo0 < periodo_anteultimo & (periodo0+1 == periodo1 )
+         & ( is.na(periodo2) | periodo0 + 2 < periodo2 ),
+         clase_ternaria := "BAJA+2" ]
+
+
+# pego el resultado en el dataset original y grabo
+setorder( dsimple, pos )
+dataset[, clase_ternaria := dsimple$clase_ternaria ]
+
+fwrite( dataset,
+        file =  "competencia_01_R.csv",
+        sep = ","
+)
